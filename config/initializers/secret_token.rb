7a41b3f7884ae33b66cc656357f35e7cb9f81fd5
Latem <latem@localhost.localdomain>
2013-07-03 13:53:54 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '0e080b69c1b0c993d5c5fe1dc6b84b77ca2277694eba50c78399551abbe2312def76f16cf8ba0172b2b5f3a8c0cafcc4ed9e7eb71a28979d1ee511add10c804b'
