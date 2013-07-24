d174e06e5b55c83f846177b0f1f9f9afb2b01c37
latem <latem@localhost.localdomain>
2013-07-24 18:08:06 +0300

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end  
+
   def new
+    @user = User.new
+  
+   def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Triforce!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
+end
 end
